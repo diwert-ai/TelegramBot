bf8a53df0f00f8711eb4ecf185abda54e94bdb5a
diwert-ai <diwert08@gmail.com>
2023-02-19 02:37:17 +0300

    обкатываем команду /start

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+from telegram.ext import Updater, CommandHandler
 from config import Config
+
+
+def on_start_command(update, context):
+    print('Start event message received!')
+
+
+def main():
+    bot = Updater(token=Config.token)
+    dp = bot.dispatcher
+    dp.add_handler(CommandHandler('start', on_start_command))
+
+    bot.start_polling()
+    bot.idle()
+
+
+if __name__ == "__main__":
+    main()
