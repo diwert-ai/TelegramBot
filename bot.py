c8669be0c0737dbf38786bbc75f4a0961bf5c846
diwert-ai <diwert08@gmail.com>
2023-02-19 03:03:43 +0300

    логирование

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
+import logging
 from telegram.ext import Updater, CommandHandler
 from config import Config
+
+logging.basicConfig(filename='bot.log',
+                    format='[%(asctime)s] [%(levelname)s] => %(message)s',
+                    level=logging.INFO)
 
 
 def on_start_command(update, context):
     print('Start event message received!')
+    print(f'update: {update}')
+    update.message.reply_text(f'Hi, {update.message.chat.username}!')
 
 
 def main():
@@ -11,6 +18,7 @@ def main():
     dp = bot.dispatcher
     dp.add_handler(CommandHandler('start', on_start_command))
 
+    logging.info('Bot run')
     bot.start_polling()
     bot.idle()
 
