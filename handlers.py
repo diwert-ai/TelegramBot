77eb1adeb4fb6c282d770981a4d4f1b4f222630f
diwert-ai <diwert08@gmail.com>
2023-02-24 16:56:49 +0300

    add handler to request ngram stats from Google Ngram Viewer service

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-from utils import gen_magic_string, get_bulls_cows_reply, is_numeric
+from utils import gen_magic_string, get_bulls_cows_reply, is_numeric, run_query
 
 
 def on_start_command(update, context):
@@ -29,6 +29,20 @@ def on_guess_command(update, context):
     else:
         message = 'Enter string of 4 digits!'
     update.message.reply_text(message)
+
+
+def on_ngram_command(update, context):
+    print('Ngram event message received!')
+    args = context.args
+    print(f'args: {args}')
+    if args:
+        user_string = args[0]
+        print(f'user_string: {user_string}')
+        stat = run_query(user_string)[0][1]
+    else:
+        stat = None
+
+    update.message.reply_text(f'{stat}')
 
 
 def on_text_message(update, context):
