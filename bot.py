00849db59101876f3263444931987668a99914f4
diwert-ai <diwert08@gmail.com>
2023-02-24 16:57:35 +0300

    add processing /ng command (ngram stats)

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config import Config
-from handlers import on_start_command, on_guess_command, on_text_message
+from handlers import on_start_command, on_guess_command, on_text_message, on_ngram_command
 
 
 logging.basicConfig(filename='bot.log',
@@ -15,6 +15,7 @@ def main():
     dp = bot.dispatcher
     dp.add_handler(CommandHandler('start', on_start_command))
     dp.add_handler(CommandHandler('g', on_guess_command))
+    dp.add_handler(CommandHandler('ng', on_ngram_command))
     dp.add_handler(MessageHandler(Filters.text, on_text_message))
 
     logging.info('Bot is running...')
