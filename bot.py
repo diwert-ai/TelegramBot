c9a7770df4552d004f4bac89af02ec845cf5bd80
diwert-ai <diwert08@gmail.com>
2023-02-24 18:21:23 +0300

    register handler for `/decode` command

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config import Config
-from handlers import on_start_command, on_guess_command, on_text_message, on_ngram_command
+from handlers import on_start_command, on_guess_command, on_text_message, on_ngram_command, on_decode_command
 
 
 logging.basicConfig(filename='bot.log',
@@ -15,7 +15,8 @@ def main():
     dp = bot.dispatcher
     dp.add_handler(CommandHandler('start', on_start_command))
     dp.add_handler(CommandHandler('g', on_guess_command))
-    dp.add_handler(CommandHandler('ng', on_ngram_command))
+    dp.add_handler(CommandHandler('ngram', on_ngram_command))
+    dp.add_handler(CommandHandler('decode', on_decode_command))
     dp.add_handler(MessageHandler(Filters.text, on_text_message))
 
     logging.info('Bot is running...')
